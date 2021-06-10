611d23f18676707d045a62f2f0350b729e159f1d
dev88jerry <70291890+dev88jerry@users.noreply.github.com>
2021-06-10 15:56:18 -0400

    Update buyBBbot.js
    test code

--- js-bot/buyBBbot.js
+++ js-bot/buyBBbot.js
@@ -1,3 +1,4 @@
+/*
 var audio;
 function doSound() {
     try{
@@ -8,29 +9,30 @@ function doSound() {
     audio = new Audio('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3');
     audio.play();
 }
+*/
 var bestBuyTimer = parseInt(localStorage.getItem('best-buy-count'))
 var secondsToRefresh = 1
 bestBuyTimer = isNaN(bestBuyTimer) ? 1 : bestBuyTimer
 setTimeout(() => {
-    //links to xbox, ps5
-	var bestBuyXboxPath = '/en-ca/product/xbox-series-x-1tb-console-new-model-online-only/14964951'
-    var bestBuyPS5Path = '/en-ca/product/playstation-5-console-online-only/14962185'
-	//links to 3060ti, 3060
-	var bestBuy3060Path = '/en-ca/product/evga-nvidia-geforce-rtx-3060-xc-12gb-dddr6-video-card/15318940'
-	var bestBuy3060tiPath = '/en-ca/product/nvidia-geforce-rtx-3060-ti-8gb-gddr6-video-card/15166285'
-	
-    //link below is no good
+        
 	var confirmNoWarranty = '/en-ca/required-parts'
 	//requires user to be logged in
     var paymentChoice = '/en-ca/basket'
-    console.log('Checking Xbox Page...')
-    if(
-        location.pathname.indexOf(bestBuyXboxPath) > -1 ||
-        location.pathname.indexOf(bestBuyPS5Path) > -1
-    ){
-        const el = document.querySelector('.addToCartButton');
-        console.log('Is Xbox Series X page')
-        if(el.disabled){
+	
+	//test code to see if in stock items work
+	
+	var djiAir2sPath = '/en-ca/product/dji-air-2s-quadcopter-drone-with-camera-controller-grey/15387566'
+	
+	var hyperXPath = '/en-ca/product/hyperx-cloud-flight-over-ear-sound-isolating-rf-wireless-gaming-headset-black-red/12330375'
+	
+	var macbookPath = '/en-ca/product/apple-macbook-pro-13-3-w-touch-bar-fall-2020-space-gray-apple-m1-chip-1tb-ssd-16gb-ram-en/15318942'
+	
+	console.log('Checking Pages...')
+	
+	if(location.pathname.indexOf(hyperXPath) > -1) {
+		const el = document.querySelector('.addToCartButton');
+		console.log('In Stock')
+		if(el.disabled){
             ++bestBuyTimer
             secondsToRefresh = (70 + bestBuyTimer)
             console.log(`Out of Stock... Reloading in ${secondsToRefresh} seconds`)
@@ -42,11 +44,54 @@ setTimeout(() => {
                 }
             },1000 * secondsToRefresh)
         }else{
-            doSound()
+            //doSound()			
             el.click()
+			alert('HyperX in Stock.... BUY BUY BUY!!!!!')
         }
-    }else if(location.pathname.indexOf(confirmNoWarranty) > -1){
-        doSound()
+	}
+	else if (location.pathname.indexOf(djiAir2sPath) > -1)
+	{
+		const el = document.querySelector('.addToCartButton');
+		console.log('In Stock')
+		if(el.disabled){
+            ++bestBuyTimer
+            secondsToRefresh = (70 + bestBuyTimer)
+            console.log(`Out of Stock... Reloading in ${secondsToRefresh} seconds`)
+            localStorage.setItem('best-buy-count',`${bestBuyTimer}`)
+            setTimeout(() => {
+                location.reload();
+                if(bestBuyTimer > 9){
+                    localStorage.setItem('best-buy-count',`1`)
+                }
+            },1000 * secondsToRefresh)
+        }else{
+            //doSound()
+            el.click()
+			alert('DJI drone in Stock.... BUY BUY BUY!!!!!')
+        }
+	}
+	else if (location.pathname.indexOf(macbookPath) > -1) {
+		const el = document.querySelector('.addToCartButton');
+		console.log('In Stock')
+		if(el.disabled){
+            ++bestBuyTimer
+            secondsToRefresh = (70 + bestBuyTimer)
+            console.log(`Out of Stock... Reloading in ${secondsToRefresh} seconds`)
+            localStorage.setItem('best-buy-count',`${bestBuyTimer}`)
+            setTimeout(() => {
+                location.reload();
+                if(bestBuyTimer > 9){
+                    localStorage.setItem('best-buy-count',`1`)
+                }
+            },1000 * secondsToRefresh)
+        }else{
+            //doSound()			
+            el.click()
+			alert('MacBook in Stock.... BUY BUY BUY!!!!!')
+        }
+	}
+	else if(location.pathname.indexOf(confirmNoWarranty) > -1){
+        //doSound()
         const el = document.querySelector('[data-automation="go-to-cart"]');
         console.log('Is Confirm Adding Page')
         if(el.disabled){
@@ -59,8 +104,8 @@ setTimeout(() => {
             el.click()
         }
     }else if(location.pathname.indexOf(paymentChoice) > -1){
-        alert('BUY THE XBOX MAN!')
-        doSound()
+        alert('BUY THE STUFF MAN!')
+        //doSound()
     }else{
         console.log('Wrong Page : ' + location.href)
     }
